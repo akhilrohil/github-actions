967ec663128b2171b261a359462d10155f93c279
Akhilesh Kumar <akhil.akhilesh@gmail.com>
2025-04-22 21:53:34 -0500

    Updated feature X

--- src/main/java/com/githubactions/testGithub/TestGithubApplication.java
+++ src/main/java/com/githubactions/testGithub/TestGithubApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestGithubApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		SpringApplication.run(TestGithubApplication.class, args);
 	}
 
