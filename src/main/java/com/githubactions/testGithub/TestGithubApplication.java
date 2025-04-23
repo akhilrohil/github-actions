ead45086764a8c75c7531f2efa89502d00f235d2
akhilrohil <31796016+akhilrohil@users.noreply.github.com>
2025-04-22 22:16:12 -0500

    Update TestGithubApplication.java
    adding directly in github

--- src/main/java/com/githubactions/testGithub/TestGithubApplication.java
+++ src/main/java/com/githubactions/testGithub/TestGithubApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestGithubApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello world");
 		SpringApplication.run(TestGithubApplication.class, args);
 	}
 
