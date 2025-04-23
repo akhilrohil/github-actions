fdb5afbed2a079d3afa90c4a0ccdcbfc7d467581
akhiluser <akhil02akhileshk@gmail.com>
2025-04-22 20:56:06 -0500

    first commit

---
+++ src/test/java/com/githubactions/testGithub/TestGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubactions.testGithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
